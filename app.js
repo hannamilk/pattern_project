94950e3ef521820f4af0e4f8d8cad916c6fbfa91
Hanna <hanna.milk@khk.ee>
2022-01-04 14:04:33 +0200

    Created the UI with Materialize CSS

---
+++ app.js
@@ -0,0 +1,15 @@
+const UIController = (function(){
+    let text = 'Hello World!'
+
+    const changeText = function(){
+        const element = document.querySelector('h1')
+        element.textContent = text
+    }
+    return {
+        callChangeText: function (){
+            changeText()
+            console.log(text)
+        }
+    }
+})();
+UIController.callChangeText()
